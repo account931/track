1d7cbd0ab90f1e7ce33a86e3902539e4acbe6379
account931 <account931@ukr.net>
2024-09-20 14:05:46 +0200

    Add files via upload
    added new countries + 'not in the list' message

--- display.php
+++ display.php
@@ -1,22 +1,26 @@
-<?php  
-//THIS  SHOWS  ALL  RECORDS ,  normal  &  Ajax//<!--check pass and  set SESSION['X']+unset when logged-->
+<?php
+ob_start(); //fix error Headers already sent
+session_start();  
+//This  file shows  ALL  RECORDS (ip addresses, time, etc from /ipTrack.txt) ,  normal  &  Ajax//<!--check pass and  set SESSION['X']+unset when logged-->
 //<!--uses ajaxscript.js+ ajaxphprequest.php-->
 ?>
 <!DOCTYPE html>
 <html>
 <head>
 <title>Admin Section</title>
-
-	<link rel="stylesheet" type="text/css" href="style.css" />
-
-
+<link rel="stylesheet" type="text/css" href="style.css" />
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
 <script src="ajaxscript.js"></script><!--AJAX script-->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-
-
-
+<!-- bootstrap -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+  
 
 <script> //click on arrow  down to  see  UserAgent  info  //&#9660
 $(document).ready(function(){  
@@ -29,14 +33,10 @@ $(document).ready(function(){
 </script>
 
 
-
-
-
-
 <style>
 .shadow2R{text-shadow: 2px 2px 4px  black/*#000000*/; }
 .boxedshadow{box-shadow: 0 0 10px rgba(0,0,0,1.5);}
-.format{width:60%;}
+.format{width:90%;}
 .imgClick:hover{cursor:pointer;color:red;}// arrow  down  style
 .imgClick{padding-left:65px;}
 .hiddennn{display:none;}
@@ -47,18 +47,20 @@ $(document).ready(function(){
 
 
 <?php
-session_start();
+
 //$_SESSION['auth']=true;   BUGGG
 
-if(!isset($_SESSION['auth'])){ ?>    
+if(!isset($_SESSION['auth'])){ 
+?>    
 
 
 <!------------------------------------------------If not authorized---------------------------------------->
-<div id="login" class="shadow2 ">
+<div id="login" class="shadow2 col-sm-12 col-xs-12">
 <!--<img scr="http://example2.esy.es/tr/images/accessd.png" alt="access"/>-->
 
 <p class="imgClick">&#9660;</p> <p style="display:none;font-size:46px;" class="hiddennn">NO ACCESS  GRANTED</p>
 </br></br>
+<p><i class="fa fa-drivers-license-o" style="font-size:24px"></i> Welcome acc***1, pass: currEmp</p>
 <form action="" method="post">
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Login:&nbsp<input type="text" name="login1" required class="resultRu">
 </br></br>
@@ -77,8 +79,8 @@ Password:&nbsp<input type="password" name="pass1" required class="resultRu">
 
 
 
-<div id="log" class="boxedshadow format">
-</br><a style="color:red;border:1px solid  red;padding:4px;" href="/tr/display.php/?logout">Log out</a></br>
+<div id="log" class="boxedshadow format col-sm-12 col-xs-12 ">
+</br><a style="color:red;border:1px solid red;padding:4px;" href="display.php/?logout">Log out</a>  <span style="color:red;font-size:1.7em;"> &#10052; </span> </br>
 <!-----------------------------------------------------------------------------------Display log----------------------------------------------------------->
 
 
@@ -87,37 +89,44 @@ Password:&nbsp<input type="password" name="pass1" required class="resultRu">
 //doing  reverse-temporary  disabled,trying  to  ressigned  to AJAX
 
 $coutriesIP = array(
-        'Lithuania'   => '188.69.232.129',
-	'Paris'       => '176.161.170.36',
+    'Brussels'    => '85.119.216.74',
+	'Bulgaria'    => '82.137.91.112',
 	'Estonia'     => '213.35.160.50',
-	'Switzerland' => '46.253.189.218',
 	'Hungary'     => '93.189.114.202',
 	'Latvia'      => '213.175.127.228',
-	'Bulgaria'    => '82.137.91.112',
-	'Nice'        => '193.57.185.10',
-	'Brussels'    => '85.119.216.74',
+	'Lithuania'   => '188.69.232.129',
 	'Netherland'  => '92.70.92.181',
-    'Turkey'      => '95.2.184.235',
+	'Nice'        => '193.57.185.10',
+	'Norway'      => '176.221.91.52',
+	'Paris'       => '176.161.170.36',
+	'Switzerland' => '46.253.189.218',
+	'Turkey'      => '5.2.184.235',
+	                  
 );
 $file = file("ipTrack.txt");
 $file = array_reverse($file);
-foreach($file as $f){
+//$copenIP = "/\93.165.147\b/";
+ $copenIP = '/93.165.147.58(\.com)?/';
+foreach($file as $f){  
     //if(trim($f) !='') {var_dump($f);}
+	
     echo $f; //display the file line itself (ip address, time, etc)
-    //display alert sign if IP is foreign(not DK)
-    if (trim($f) !='' && !preg_match('/93.165.147.58|82.163.120.154|152.115.135.54/', $f)) { 
-	    echo '<span style="color:red;font-size:0.6em;">foreign </span>'; 
+	//display alert sign if IP is foreign(not DK)
+	if (trim($f) !='' && !preg_match('/93.165.147.58|82.163.120.154|152.115.135.54/', $f)) { 
+	    echo '<span style="color:red;font-size:0.5em;">foreign </span> 
+		     <span style="color:red;font-size:0.7em;"> &#9730; : </span>';   //html icon
 		//dispaly foreign country if it is in $coutriesIP
+		$detectedCountry = "not in the list";
 		foreach($coutriesIP as $key => $val){ 
 			if(preg_match("/$val/", $f)) { 
-			    echo '<span style="color:red; font-size:0.6em;">' .  $key . '</span>'; 
+			    $detectedCountry = $key;
 			}
 			
 		}
-	}	
+		echo '<span style="color:red; text-decoration: underline; font-size:0.6em;">' .  $detectedCountry . '</span>'; 
+	}
 	echo "<br/>";
-	//if (trim($f) !='' && !preg_match('/93.165.147.58/', $f)) { echo '<span style="color:red;">foreign </span>'; }
-	//if (str_contains('93.165.147.58', $f)) { echo 'foreign '; }
+
 }
 
 
@@ -160,25 +169,24 @@ foreach($file as $f){
 <?php
 //checking password
 if(isset($_POST['pass1'])){
-   if   ($_POST['login1']=='account***' && $_POST['pass1']=='v***') 
- {$_SESSION['auth']=true;echo'true';header ('Location: http://example2.esy.es/tr/display.php');
-                                    } else {echo'<p style="color:red;">Failed</p>';}
+    if($_POST['login1']=='account931' && $_POST['pass1']=='loyalty') {
+        $_SESSION['auth']=true;echo'true';
+		header ('Location: display.php');
+    } else {
+		echo'<p style="color:red;">Failed</p>';
+	}
 
 } // End  if(isset($_POST['pass1']))
 
 
 //Log OUT
 if(isset($_GET['logout'])){
-session_destroy();unset($_SESSION['auth']); header ("Location: http://example2.esy.es/tr/display.php");
+    session_destroy();
+	unset($_SESSION['auth']); 
+	header ("Location: display.php");
 }
-
+ob_end_flush(); //fix error Headers already sent
 ?>
-
-
-
-
-
-
 
 
 </center>
